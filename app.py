f75d22b8d1554be842bc3a68e4c220029b77a3ce
1984ejp <1984ejp@gmail.com>
2026-04-15 12:56:39 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -138,8 +138,7 @@ with col2:
 if opcion == "ANTES DE MI ENDOSCOPIA":
     st.markdown("## 📋 Antes de mi Endoscopía")
     st.markdown(f'<div style="background-color:white; padding:30px; border-radius:15px; border-left:10px solid #2bb673; color:#333333 !important; font-size:18px; line-height:1.7;">{TEXTO_ANTES.replace(chr(10), "<br>")}</div>', unsafe_allow_html=True)
-    st.markdown("### 🍎 Dieta 3 días previos")
-    mostrar_docx("textos/Dieta comun 3 días PREVIOS AL ESTUDIO.docx")
+    st.markdow
 
 elif opcion == "MI PREPARACIÓN":
     st.markdown("## 💧 Plan de Preparación")
